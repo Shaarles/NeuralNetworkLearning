79597c3509d1dc10aa10bf88eb775e53449bd672
Charles Guo <charlesguo91@gmail.com>
2026-02-23 15:50:07 +0100

    Ajoutez des fichiers projet.

---
+++ Network.cpp
@@ -0,0 +1,26 @@
+﻿#include "Vecteur.cpp"
+#include "Network.h"
+
+
+class Network{
+private:
+	int size;
+	int* dimensions;
+	Vector* weights;
+	Vector* biases;
+
+public:
+
+	Network(int size, int* dimensions) {
+		this->size = size;
+		this->dimensions = dimensions;
+		this->weights = (Vector*)malloc(size*sizeof(Vector));
+		this->biases = (Vector*)malloc(size * sizeof(Vector));
+	}
+
+	~Network() {
+		free(this->weights);
+		free(this->biases);
+	}
+
+};
---
+++ Vecteur.cpp
@@ -0,0 +1,5 @@
+#include "Vecteur.h"
+
+class Vector {
+
+};
